b07846c466cb6931f0860eeb40f236841a3c794d
Ernesto-B <ernesto.barreto@ucalgary.ca>
2023-01-15 20:17:53 -0700

    parsed each container

--- client.py
+++ client.py
@@ -17,4 +17,7 @@ uClient.close()
 # Now we need to parse the HMTL using bs4
 page_soup = soup(page_html, "html.parser")
 
-# Now we want to traverse the html
+# Now we want to traverse the html and find the container objects of each anime
+containers = page_soup.findAll("div", {"class": "js-anime-category-producer seasonal-anime js-seasonal-anime js-anime-type-all js-anime-type-1"})
+
+print(len(containers))
