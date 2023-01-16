fdb084b112d61f9f25bc236527eaaf0bbfeab748
Ernesto-B <ernesto.barreto@ucalgary.ca>
2023-01-15 19:55:22 -0700

    made connection and got parsed html

--- client.py
+++ client.py
@@ -1,2 +1,18 @@
-from urllib import request
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import csv
 
+# URL to scrape ... currently set to the anime season page on MAL
+url_anime = "https://myanimelist.net/anime/season"
+
+# Open connection, grab page
+uClient = uReq(url_anime)
+
+# Read page
+page_html = uClient.read()
+
+# Close connection
+uClient.close()
+
+# Now we need to parse the HMTL using bs4
+page_soup = soup(page_html, "html.parser")
